b62d1f99bcd87927a5f5740645bea95c79b0d9e9
polzak <hello.polzak@gmail.com>
2015-09-24 11:05:14 +0900

    ready for middleware that could do authentication and validation

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://127.0.0.1/crm', function(){
 	console.log("mongodb connected!");
 });
 
-//body parser를 이용해 포스트로 요청(request)되는 정보를 잡아온다.
+//body parser를 이용해 포스트로 요청(request)되는 정보를 파싱한다.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -30,7 +30,22 @@ app.get('/', function(req, res){
   res.send('Welcome to the home page!');
 });
 
+//익스프레스 라우터 인스턴스 호출.
 var apiRouter = express.Router();
+
+//***매우 중요 ***
+//모든 리퀘스트에 대한 미들웨어 설정.
+//이러한 미들웨어를 설정하면 굉장히 강력해진다. 모든 요청이 안전한지 검증할 수 있기 때문이다.
+//또한 로그 통계 분석을 하는 미들웨어를 추가할 수도 있다.
+//무엇보다도 인증 기능이 가장 중요할 것이다. 유저가 적절한 토큰을 가지고 있는지 인증할 수 있다.
+apiRouter.use(function(req, res, next){
+	//누군가 로그한다.
+	console.log('Somebody just came to our app!');
+
+	//이후 이 부분에서 인증 설정을 할 것이다.
+
+	next();//다시 하던 라우팅 일 계속해라. (미들웨어가 더 있으면 그거 계속하고.)
+});
 
 //test route
 apiRouter.get('/', function(req, res){
