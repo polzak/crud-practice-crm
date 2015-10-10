6c11d8edd414fd3f14a87c24df6da0162a26919f
polzak <hello.polzak@gmail.com>
2015-10-10 17:14:47 +0900

    create the login page and set the routing for it

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -6,6 +6,12 @@ angular.module('app.routes', ['ngRoute'])
 
       .when('/', {
         templateUrl: '/app/views/pages/home.html'
+      })
+
+      .when('/login', {
+        templateUrl: '/app/views/pages/login.html',
+        controller: 'mainController',
+        controllerAs: 'login'//로그인에 관한 한 다르게 부를 수 있도록.
       });
 
        $locationProvider.html5Mode(true);
