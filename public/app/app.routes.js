e0586b7dc7c3a61489becd95d401709dd561a7bd
polzak <hello.polzak@gmail.com>
2015-10-11 15:02:29 +0900

    create CreateUser function in userCtrl and html page

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -18,7 +18,14 @@ angular.module('app.routes', ['ngRoute'])
         templateUrl: '/app/views/pages/users/all.html',
         controller: 'userController',
         controllerAs: 'user'
+      })
+
+      .when('/users/create', {
+        templateUrl: '/app/views/pages/users/single.html',
+        controller: 'userCreateController',
+        controllerAs: 'user'
       });
+
 
        $locationProvider.html5Mode(true);
   });
--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -12,7 +12,7 @@ angular.module('userCtrl', ['userService'])//userService를 주입한 후
       });
 
     vm.deleteUser = function(id){
-      
+
       vm.processing = true;
 
       User.delete(id)
@@ -24,6 +24,21 @@ angular.module('userCtrl', ['userService'])//userService를 주입한 후
             });
         });
     };
+  })
+  .controller('userCreateController', function(User){
+    var vm = this;
 
+    vm.type = 'create';
 
+    vm.saveUser = function(){
+      vm.processing = true;
+      vm.message = '';
+
+      User.create(vm.userData)
+        .success(function(data){
+          vm.processing = false;
+          vm.userData = {};
+          vm.message = data.message;
+        });
+    };
   });
